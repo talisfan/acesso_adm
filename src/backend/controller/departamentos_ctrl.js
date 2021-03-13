563d6c3ae2d91e0d4eec488166b3c3b50db773c2
talisfan <talisfan@outlook.com>
2021-03-13 02:22:27 -0300

    Removendo tratamento de querys desnecessário.

--- src/backend/controller/departamentos_ctrl.js
+++ src/backend/controller/departamentos_ctrl.js
@@ -1,21 +1,12 @@
 const model = require('../model/departamentos_mdl');
-const queryAccepted = require('../middleware/queryAccepted');
 
 exports.getAllDepart = async(req, res, next)=>{
-    await model.getAllDepart(req, res, next);
+    return await model.getAllDepart(req, res, next);
 }
 
 exports.createDepart = async(req, res, next)=>{
-    if(req.body && req.body.nomeDepart){
-
-        if(!queryAccepted(req.body.nomeDepart)){        
-            await model.createDepart(req, res, next);
-        }else{
-            return next(new Error({     
-                status: 400,              
-                errorMessage: 'Property "nomeDepart" contains invalid characters.'
-            }));
-        }        
+    if(req.body && req.body.nomeDepart){             
+        return await model.createDepart(req, res, next);                
     }else{        
         return next(new Error({     
             status: 400,              
@@ -26,16 +17,8 @@ exports.createDepart = async(req, res, next)=>{
 
 exports.attDepart = async(req, res, next)=>{
       
-    if(req.body && req.body.nomeDepart && req.body.idDepart && req.body.idDepart > 0){
-
-        if(!queryAccepted(req.body.nomeDepart)){        
-            await model.attDepart(req, res, next);
-        }else{
-            return next(new Error({     
-                status: 400,              
-                errorMessage: 'Property "nomeDepart" contains invalid characters.'
-            }));
-        }
+    if(req.body && req.body.nomeDepart && req.body.idDepart && req.body.idDepart > 0){        
+        return await model.attDepart(req, res, next);        
     }else{
         return next(new Error({     
             status: 400,              
@@ -45,8 +28,8 @@ exports.attDepart = async(req, res, next)=>{
 }
 
 exports.deleteDepart = async(req, res, next)=>{    
-    if(req.body && req.params.idDepart && req.params.idDepart > 0){
-        await model.deleteDepart(req, res, next);
+    if(req.params && req.params.idDepart && req.params.idDepart > 0){        
+        return await model.deleteDepart(req, res, next);                
     }else{
         return next(new Error({     
             status: 400,              
