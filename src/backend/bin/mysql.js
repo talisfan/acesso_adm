9bba5a37d4f2146de3f45ff3d414c23965328dbc
talisfan <talisfan@outlook.com>
2021-03-15 02:52:18 -0300

    Add logica de ambientes

--- src/backend/bin/mysql.js
+++ src/backend/bin/mysql.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql');
 
-var pool = mysql.createPool({
-    "user": "root",
-    "password" : "root",
-    "database": "db_controle",
-    "host": "127.0.0.1"
+require('dotenv').config({  
+    path: process.env.ENV === "dev" ? ".env.dev" : ".env"
 });
 
-exports.pool = pool;
+console.log(process.env.ENV != "dev" ? "\nAMBIENTE: prod" : "\nAMBIENTE: "+process.env.ENV);
+
+const pool = mysql.createPool({
+    "user": process.env.DB_USER,
+    "password" : process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "port": process.env.DB_PORT,
+    "host": process.env.DB_HOST
+});
+
+module.exports = pool;
