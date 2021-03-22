a0c3863fe3a96302c89ae092bd31cf5b625344a1
talisfan <talisfan@outlook.com>
2021-03-22 02:39:56 -0300

    Att formatacao.

--- src/backend/utils/script_bd.sql
+++ src/backend/utils/script_bd.sql
@@ -1,22 +1,26 @@
-drop database acesso_adm;
-create database acesso_adm default character set utf8 collate utf8_unicode_ci;
+#use mysql;
+#update user set authentication_string=password('root'), plugin='mysql_native_password' where user='talis';
+#FLUSH PRIVILEGES;
+
+drop database if exists acesso_adm;
+create database if not exists acesso_adm default character set utf8 collate utf8_unicode_ci;
 use acesso_adm;
 
 create table if not exists tbl_departamentos(
-idDepart integer not null primary key auto_increment,
-nomeDepart varchar(200) not null unique
+	idDepart integer not null primary key auto_increment,
+	nomeDepart varchar(200) not null unique
 );
 
 create table if not exists tbl_funcionarios(
-id integer not null primary key auto_increment,
-nome varchar(200) not null,
-telefone varchar(15) not null,
-email varchar(200) not null unique,
-acesso enum('ADM', 'FUNCIONÁRIO', 'CLIENTE'),
-senha varchar(100) not null,
-ativo bit not null default(1), ## 1=ativo; 0=inativo
-idDepartamento integer not null,
-FOREIGN KEY (idDepartamento) REFERENCES tbl_departamentos (idDepart)
+	id integer not null primary key auto_increment,
+	nome varchar(200) not null,
+	telefone varchar(15) not null,
+	email varchar(200) not null unique,
+	acesso enum('ADM', 'FUNCIONÁRIO', 'CLIENTE'),
+	senha varchar(100) not null,
+	ativo bit not null default 1, ## 1=ativo; 0=inativo
+	idDepart integer not null,
+	FOREIGN KEY (idDepart) REFERENCES tbl_departamentos (idDepart)
 );
 
 ##trigger para deletar departamento
@@ -25,7 +29,7 @@ CREATE TRIGGER trigger_depart before delete
 ON tbl_departamentos
 FOR EACH ROW
 BEGIN
-	update tbl_funcionarios set idDepartamento = 1 where idDepartamento = old.idDepart;
+	update tbl_funcionarios set idDepart = 1 where idDepart = old.idDepart;
 END$
 DELIMITER ;
 
@@ -39,10 +43,10 @@ values (2, "TI");
 
 select * from tbl_departamentos;
 
-insert into tbl_funcionarios (nome, telefone, email, senha, acesso, idDepartamento)
+insert into tbl_funcionarios (nome, telefone, email, senha, acesso, idDepart)
 values ("Talisson Maciel Luques", "(11) 95118-4349", "talis@talis.com", "123456", "ADM", 1);
 
 select * from tbl_funcionarios;
 
 select f.nome, d.nomeDepart as DEPARTAMENTO from tbl_funcionarios f
-inner join tbl_departamentos d on (d.idDepart = f.idDepartamento);
+inner join tbl_departamentos d on (d.idDepart = f.idDepart);
