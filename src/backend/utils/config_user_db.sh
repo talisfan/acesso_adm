405df7575c3a153bfff4782da1eaf4aed9868471
talisfan <talisson-luques@hotmail.com>
2021-05-04 23:32:11 -0300

    fix: Config pass of user for mysql.

--- src/backend/utils/config_user_db.sh
+++ src/backend/utils/config_user_db.sh
@@ -1,7 +1,9 @@
-CREATE USER $MYSQL_USER@"localhost" IDENTIFIED BY $MYSQL_PASSWORD;
-GRANT ALL PRIVILEGES ON * . * TO $MYSQL_USER@"localhost";
+#CREATE USER "talis"@"localhost" IDENTIFIED BY "root";
+ALTER USER "talis"@"localhost" IDENTIFIED WITH mysql_native_password BY "root";
+GRANT ALL PRIVILEGES ON * . * TO "talis"@"localhost";
 
-CREATE USER $MYSQL_USER@"%" IDENTIFIED BY $MYSQL_PASSWORD;
-GRANT ALL PRIVILEGES ON * . * TO $MYSQL_USER@"%";
+#CREATE USER "talis"@"%" IDENTIFIED BY "root";
+ALTER USER "talis"@"%" IDENTIFIED WITH mysql_native_password BY "root";
+GRANT ALL PRIVILEGES ON * . * TO "talis"@"%";
 
 FLUSH PRIVILEGES;
