dd2f717637a6ada36cef8fb6f211fd21e6a3d285
talisfan <talisson-luques@hotmail.com>
2021-05-12 00:58:43 -0300

    fix: Corrigindo nomenclatura.

--- src/frontend/assets/javascript/validacoes.js
+++ src/frontend/assets/javascript/validacoes.js
@@ -23,25 +23,3 @@ $(function () {
 	});//function de dentro
 });//function de fora
 
-$(function () {
-	$('#cad').submit(function () {
-		var obj = this;
-		var form = $(obj);
-		var dados = new FormData(obj);
-		$.ajax({
-			url: form.attr('action'),
-			type: form.attr('method'),
-			data: dados,
-			processData: false,
-			cache: false,
-			contentType: false,
-			success: function (data) {
-				var divCad = document.getElementById("divCad");
-				divCad.className = "p-2 mx-auto border border-secondary rounded col-md-6 col-11 my-5 shadow-lg py-4 animated fadeInDown";
-				var divEscolha = document.getElementById("divEscolha");
-				divEscolha.className = "d-none";
-			},
-		});
-		return false;
-	});
-});
