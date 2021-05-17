17252fda773f8850aa8fcf39f7145ff7945d7e10
talisfan <talisson-luques@hotmail.com>
2021-05-16 23:46:53 -0300

    fix: Corrigindo tratamento de erros no response.

--- src/frontend/assets/javascript/utils_functions.js
+++ src/frontend/assets/javascript/utils_functions.js
@@ -22,19 +22,24 @@ function getQueryStrings(){
     return null;
 }
 
-async function treatmentErrorResponse(res){
+async function treatmentErrorResponse(res, entity){
     res = await res.json();									
 
     if(
         res.error && res.errorDescription.errorMessage.code &&
         res.errorDescription.errorMessage.code == 'ER_DUP_ENTRY'
     ){								
-        genericErrors('Departamento já existente');										
+        genericErrors(`${entity} já existente`);										
     }else if(
         res.error && res.errorDescription.errorMessage &&
         res.errorDescription.errorMessage.includes('Missing required')
     ){
         genericErrors('Preencha todos os campos obrigatórios!');	
+    }else if(
+        res.error && res.errorDescription.errorMessage &&
+        res.errorDescription.errorMessage.includes('senhas')
+    ){
+        genericErrors(res.errorDescription.errorMessage);	
     }else{
         res = JSON.stringify(res);
         res = btoa(res);
