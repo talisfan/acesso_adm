953dff146d7e5f8096041bca33ebe44921b3b91e
talisfan <talisson-luques@hotmail.com>
2021-05-23 01:10:11 -0300

    fix: Add function queryAccepted in utils_functions file.

--- src/backend/middleware/queryAccepted.js
+++
@@ -1,8 +0,0 @@
-module.exports = (query)=>{
-    if(query.includes('"') || query.includes("'") || query.includes(";") || query.includes("#") || query.includes("%")){
-        return false;
-    }else{
-        return true;
-    }
-}
-
