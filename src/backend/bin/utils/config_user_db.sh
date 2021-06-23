9027e95a119abe8e0ddc5616590693b57da2b9ae
talisfan <talisson-luques@hotmail.com>
2021-06-23 01:34:03 -0300

    fix: Remove unnecessary file.

--- src/backend/bin/utils/config_user_db.sh
+++
@@ -1,9 +0,0 @@
-#CREATE USER "talis"@"localhost" IDENTIFIED BY "root";
-ALTER USER "talis"@"localhost" IDENTIFIED WITH mysql_native_password BY "root";
-GRANT ALL PRIVILEGES ON * . * TO "talis"@"localhost";
-
-#CREATE USER "talis"@"%" IDENTIFIED BY "root";
-ALTER USER "talis"@"%" IDENTIFIED WITH mysql_native_password BY "root";
-GRANT ALL PRIVILEGES ON * . * TO "talis"@"%";
-
-FLUSH PRIVILEGES;
