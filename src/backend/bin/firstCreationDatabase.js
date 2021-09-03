b04abfa03306e7f9ba5399070f91dfec50deae7f
talisfan <talisson-luques@hotmail.com>
2021-09-03 01:51:16 -0300

    feat: Finalizando criação do banco inicial

--- src/backend/bin/firstCreationDatabase.js
+++ src/backend/bin/firstCreationDatabase.js
@@ -5,20 +5,21 @@ module.exports = async()=>{
     await static.utils_functions.sleep(4000);    
     let tentativas = 0;
     
-    while(tentativas < 3){       
+    while(tentativas < 5){       
         try{
             let queryValue = {
                 query: `SELECT idDepart FROM ${static.strings.TABLE_DEPARTAMENTOS};`,
                 values: null
             }
-            await services.databaseConn(queryValue);            
+            await services.databaseConn(queryValue);    
+            console.log('Banco de dados já criado.')        
             return;
         }catch(error){            
             if(error.errorMessage.code === 'ER_NO_SUCH_TABLE'){ 
                 break;
-            }else if(tentativas < 2){
+            }else if(tentativas < 4){
                 tentativas++;
-                await static.utils_functions.sleep(1000);
+                await static.utils_functions.sleep(2500);
                 continue;
             }else{
                 console.error('===== ERROR =====');
@@ -27,32 +28,30 @@ module.exports = async()=>{
             }
         }
     }
+        
+    console.log('\nCriando banco de dados...\n');
+    const listQuerys = require('./utils/script_db.min');
 
-    tentativas = 0;
-    
-    while(tentativas < 3){      
-        console.log('\nCriando banco de dados...\n');
-        const listQuerys = require('./utils/script_db.min');
+    let arrayTables = listQuerys.tables.split(';');
 
-        let arrayTables = listQuerys.tables.split(';');
+    for(let query of arrayTables){
+        query += ';';   
+        let queryValue_tables = { query, values: null }                     
+        await services.databaseConn(queryValue_tables);                    
+    };
 
-        for(let query of arrayTables){
-            query += ';';   
-            let queryValue_tables = { query, values: null }                     
-            await services.databaseConn(queryValue_tables);                    
-        };
+    let queryValue_trigger = {
+        query: listQuerys.trigger,
+        values: null
+    }    
+    await services.databaseConn(queryValue_trigger);       
 
-        let queryValue_trigger = listQuerys.trigger;        
+    let arrayInserts = listQuerys.inserts.split(';');
 
-        let arrayInserts = listQuerys.inserts.split(';');
-
-        for(let query of arrayInserts){
-            query += ';';                
-            let queryValue_inserts = { query, values: null }                             
-            await services.databaseConn(queryValue_inserts);   
-        };   
-        break;           
-    }
-    
+    for(let query of arrayInserts){
+        query += ';';                
+        let queryValue_inserts = { query, values: null }                             
+        await services.databaseConn(queryValue_inserts);   
+    };               
     return;
 }
